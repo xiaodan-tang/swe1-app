66903aca8389c4dffe4eaad94791f1f83440f269
xiaodan-tang <shonna.x.tang@gmail.com>
2020-10-18 00:07:28 -0400

    .

--- polls/views.py
+++ polls/views.py
@@ -52,7 +52,8 @@ class ResultsView(generic.DetailView):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/results.html', {'question': question})
 #
-#
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
